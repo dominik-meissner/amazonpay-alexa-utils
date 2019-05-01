adf7f8016d2d6ceaf62f400b1dfecb17b114c313
Daniel Neu <daneu@amazon.com>
2019-05-01 21:01:24 +0200

    WIP permissionCard

--- src/AmazonPay.ts
+++ src/AmazonPay.ts
@@ -1,10 +1,22 @@
-import { RequestEnvelope } from 'ask-sdk-model';
+import { Response, RequestEnvelope } from 'ask-sdk-model';
+import { ResponseBuilder } from 'ask-sdk';
 
 import { ChargeDirectiveBuilder } from './checkout/charge/ChargeDirectiveBuilder';
 import { ChargePayloadBuilder } from './checkout/charge/ChargePayloadBuilder';
 import { PermissionManager } from './checkout/PermissionManager';
 import { SetupDirectiveBuilder } from './checkout/setup/SetupDirectiveBuilder';
 import { SetupPayloadBuilder } from './checkout/setup/SetupPayloadBuilder';
+
+export function askForPermissionCard(
+  responseBuilder: ResponseBuilder,
+  speechText: string,
+  additionalPermissions?: Array<string>,
+): Response {
+  return PermissionManager.get()
+    .withSpeechText(speechText)
+    .withAdditionalPermissions(additionalPermissions)
+    .send(responseBuilder);
+}
 
 export function chargeDirective(payloadBuilder: ChargePayloadBuilder, token: string): ChargeDirectiveBuilder {
   return new ChargeDirectiveBuilder(payloadBuilder, token);
--- src/checkout/PermissionManager.ts
+++ src/checkout/PermissionManager.ts
@@ -1,4 +1,6 @@
-import { RequestEnvelope } from 'ask-sdk-model';
+import { Response, RequestEnvelope } from 'ask-sdk-model';
+import { ResponseBuilder } from 'ask-sdk';
+
 export class PermissionManager {
   public static get(): PermissionManager {
     if (!PermissionManager.instance) {
@@ -10,6 +12,10 @@ export class PermissionManager {
 
   private constructor() {}
 
+  private payPermission: string = 'payments:autopay_consent';
+  private speechText: string = ''; // TODO, improve this
+  private permissions: Array<string> = [];
+
   public isPurchasingAndPayEnabled(requestEnvelope: RequestEnvelope): boolean {
     const permissions = requestEnvelope.context.System.user.permissions;
     if (permissions && permissions.scopes) {
@@ -17,5 +23,23 @@ export class PermissionManager {
       return pay.status === 'GRANTED';
     }
     return false;
+  }
+
+  public withSpeechText(speechText: string): PermissionManager {
+    this.speechText = speechText;
+    return this;
+  }
+
+  public withAdditionalPermissions(permissions: Array<string> = []): PermissionManager {
+    this.permissions = permissions;
+    return this;
+  }
+
+  public send(responseBuilder: ResponseBuilder): Response {
+    const permissions = this.permissions.concat(this.payPermission);
+    return responseBuilder
+      .speak(this.speechText)
+      .withAskForPermissionsConsentCard(permissions)
+      .getResponse();
   }
 }
